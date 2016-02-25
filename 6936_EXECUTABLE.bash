3025b46165fb72d9186027cfbeed095a3da0eb4f
C5T2B7U <brian.sumner@ucdenver.edu>
2016-02-25 14:54:45 -0700

    READ THE README FILE

--- 6936_EXECUTABLE.bash
+++ 6936_EXECUTABLE.bash
@@ -1,4 +1,4 @@
 #/bin/bash
 
-g++ -o ./EMBRYO ./6936_HELIX_HW5.cpp && ./EMBRYO && make && ./6936HW_exec
+g++ -o ./EMBRYO ./6936_HELIX_HW*.cpp && ./EMBRYO && make && ./6936HW_exec
 rm -f ./6936HW* ./*.o  ./Makefile ./EMBRYO ./*.HELIX > /dev/null
